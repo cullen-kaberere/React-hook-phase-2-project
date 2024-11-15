cc01d65043e75a798b648ba05a2914c7b73d51dd
cullen kaberere <kabesh86@gmail.com>
2024-11-15 05:00:20 +0300

    change budget form

--- src/Component/BudgetForm.js
+++ src/Component/BudgetForm.js
@@ -1,33 +1,65 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './BudgetForm.css'; // Import the CSS file for styling
+import './BudgetForm.css';
 
 function BudgetForm({ setBudget }) {
-  const [input, setInput] = useState('');
+  const [title, setTitle] = useState('');
+  
+  const [amount, setAmount] = useState('');
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setBudget(parseFloat(input));
-    setInput('');
-    navigate('/add-expense'); // Navigate to "Add Expenses" page
+    const budgetData = {
+      title,
+      
+      amount: parseFloat(amount),
+    };
+
+    try {
+      // Save budget data to db.json
+      await fetch('http://localhost:3000/budgets', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(budgetData),
+      });
+      setBudget(parseFloat(amount));
+      setTitle('');
+      
+      setAmount('');
+      navigate('/add-expense');
+    } catch (error) {
+      console.error('Failed to save budget data:', error);
+    }
   };
 
   return (
     <div className="budget-form-container">
-      
       <div className="welcome-box">
         <h2>Hello.</h2>
         <p>Welcome to Ck Budget Master. Track, save, and thrive with our easy-to-use budgeting tool.</p>
         <form onSubmit={handleSubmit} className="budget-form">
-          <label htmlFor="budget-input">Please Enter Your Budget:</label>
+          <label htmlFor="title">Please Enter Your Budget Title:</label>
+          <input
+            id="title"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Enter budget title"
+            required
+          />
+
+          
+
+          <label htmlFor="budget-input">Please Enter Your Budget Amount:</label>
           <div className="input-group">
-            
             <input
               id="budget-input"
               type="number"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
               placeholder="Enter Your Budget in Ksh"
               required
             />
